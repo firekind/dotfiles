0d0b19ea0fdda9f9a6501a52dee03fe247c83086
firekind <shyamant.achar@gmail.com>
2021-02-15 13:35:21 +0530

    updated configs

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -32,7 +32,6 @@ import XMonad.Util.SpawnOnce
 
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
-
 --
 myTerminal :: String
 myTerminal = "alacritty"
